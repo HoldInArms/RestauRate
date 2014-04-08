e5ccd8ccbdf26eada8704e27a508677eff0edd2a
Gergely Peidl <geri@peidl.net>
2014-04-08 08:03:53 +0200

    Bug fix: #55 Modify the arrive time, only future could be selected! Removed datetimepicker plug in, now we use ui-bootstrap's date & time picker Added Hungarian locale.

--- api/src/main/resources/assets/lib/ui-bootstrap-tpls-0.10.0.js
+++ api/src/main/resources/assets/lib/ui-bootstrap-tpls-0.10.0.js
@@ -3635,12 +3635,12 @@ angular.module("template/timepicker/timepicker.html", []).run(["$templateCache",
   $templateCache.put("template/timepicker/timepicker.html",
     "<table>\n" +
     "	<tbody>\n" +
-    "		<tr class=\"text-center\">\n" +
+    "		<!-- <tr class=\"text-center\">\n" +
     "			<td><a ng-click=\"incrementHours()\" class=\"btn btn-link\"><span class=\"glyphicon glyphicon-chevron-up\"></span></a></td>\n" +
     "			<td>&nbsp;</td>\n" +
     "			<td><a ng-click=\"incrementMinutes()\" class=\"btn btn-link\"><span class=\"glyphicon glyphicon-chevron-up\"></span></a></td>\n" +
     "			<td ng-show=\"showMeridian\"></td>\n" +
-    "		</tr>\n" +
+    "		</tr> -->\n" +
     "		<tr>\n" +
     "			<td style=\"width:50px;\" class=\"form-group\" ng-class=\"{'has-error': invalidHours}\">\n" +
     "				<input type=\"text\" ng-model=\"hours\" ng-change=\"updateHours()\" class=\"form-control text-center\" ng-mousewheel=\"incrementHours()\" ng-readonly=\"readonlyInput\" maxlength=\"2\">\n" +
@@ -3651,12 +3651,12 @@ angular.module("template/timepicker/timepicker.html", []).run(["$templateCache",
     "			</td>\n" +
     "			<td ng-show=\"showMeridian\"><button type=\"button\" class=\"btn btn-default text-center\" ng-click=\"toggleMeridian()\">{{meridian}}</button></td>\n" +
     "		</tr>\n" +
-    "		<tr class=\"text-center\">\n" +
+    "		<!-- <tr class=\"text-center\">\n" +
     "			<td><a ng-click=\"decrementHours()\" class=\"btn btn-link\"><span class=\"glyphicon glyphicon-chevron-down\"></span></a></td>\n" +
     "			<td>&nbsp;</td>\n" +
     "			<td><a ng-click=\"decrementMinutes()\" class=\"btn btn-link\"><span class=\"glyphicon glyphicon-chevron-down\"></span></a></td>\n" +
     "			<td ng-show=\"showMeridian\"></td>\n" +
-    "		</tr>\n" +
+    "		</tr>\n -->" +
     "	</tbody>\n" +
     "</table>\n" +
     "");
